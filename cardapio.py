f05227c9f86cdde9e30b7ce759051443e7579ad2
marceloheiffigjr <marceloheiffigjr@gmail.com>
2025-11-18 23:32:59 +0000

    enviando o projeto

---
+++ cardapio.py
@@ -0,0 +1,11 @@
+def buscar_cardapio():
+    itens = [
+        {
+            "id": 1,
+            "nome": "Chocomoça",
+            "descricao": "Bolo de chocolate com leite ninho",
+            "valor": "49.99",
+            "imagem": ""
+        }
+    ]
+    return itens
